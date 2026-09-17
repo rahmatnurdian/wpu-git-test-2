d13f1d5b0baa524d9f5cf468a50dc4aedab89bbb
rahmatnurdian <rahmat.nurdian15@gmail.com>
2026-09-17 16:16:31 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Welcome to My Website</h1>
+    <p>This is a simple HTML page served by PHP.</p>
+</body>
+
+</html>
